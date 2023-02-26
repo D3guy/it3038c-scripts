c712bc5786854953b1f2f7cf353e5a2ed7bc6900
D3guy <81637083+D3guy@users.noreply.github.com>
2023-02-26 15:37:23 -0500

    Create snake.py

---
+++ project1/snake.py
@@ -0,0 +1,25 @@
+print ("how long would you like your snake to be? Enter a number between 1-10")
+
+input1 = input()
+input1 = int(input1)
+
+if input1 == 1:
+    print ("<=>-<")
+elif input1 == 2:
+    print ("<==>-<")
+elif input1 == 3:
+    print ("<===>-<")
+elif input1 == 4:
+    print ("<====>-<")
+elif input1 == 5:
+    print ("<=====>-<")
+elif input1 == 6:
+    print ("<======>-<")
+elif input1 == 7:
+    print ("<=======>-<")
+elif input1 == 8:
+    print ("<========>-<")
+elif input1 == 9:
+    print ("<=========>-<")
+elif input1 == 10:
+    print ("<==========>-<")
